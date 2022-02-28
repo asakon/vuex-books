7cc29a00e0c76bf5629eb7daac61ffa934a47c45
Asako Kondo <kondoasako@gmail.com>
2022-02-28 13:59:20 +0900

    ゲッター

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,33 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    books: [
+      {
+        isbn: 'aaa-a-aaaa-aaaa-a',
+        title: 'ねこの散歩みち',
+        price: '2300'
+      },
+      {
+        isbn: 'bbb-b-bbbb-bbbb-b',
+        title: '犬が３匹出会ったら',
+        price: '2100'
+      },
+      {
+        isbn: 'ccc-c-cccc-cccc-c',
+        title: 'まいご文鳥騒動記',
+        price: '3000'
+      },
+    ]
+  },
+  getters: {
+    booksCount(state) {
+      return state.books.length
+    },
+    getBookByPrice(state) {
+      return price => {
+        return state.books.filter(book => book.price < price)
+      }
+    }
   },
   mutations: {
   },
