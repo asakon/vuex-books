59e879e0f971a13ececb41feed7a05c673f66383
Asako Kondo <kondoasako@gmail.com>
2022-02-28 14:16:01 +0900

    コンポーネントからゲッターを参照する

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>書籍は全部で{{ booksCount }}冊あります。</p>
+    <ul v-for="b of getBookByPrice(4000)" v-bind:key="b.isbn">
+      <li>{{ b.title }} {{ b.price }}円<br>ISBN: {{ b.isbn }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { mapGetters } from 'vuex'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  computed: mapGetters([ 'booksCount', 'getBookByPrice' ])
 }
 </script>
 
