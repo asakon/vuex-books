f91d9ec6cfb30f42486bb3fe726db28f3732ac7f
Asako Kondo <kondoasako@gmail.com>
2022-03-01 11:11:07 +0900

    スタイル

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div id="app">
     <p>書籍は全部で{{ booksCount }}冊あります。</p>
-    <ul v-for="b of getBookByPrice(4000)" v-bind:key="b.isbn">
-      <li>{{ b.title }} {{ b.price }}円<br>ISBN: {{ b.isbn }}</li>
-    </ul>
+    <div class="list-wrapper">
+      <ul v-for="b of getBookByPrice(4000)" v-bind:key="b.isbn">
+        <li>『{{ b.title }}』 {{ b.price }}円（税別） (ISBN: {{ b.isbn }})</li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -24,5 +26,23 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.list-wrapper {
+  margin: 0 auto;
+  padding: 34px 0 10px;
+  width: 800px;
+  background-color: #ffe7eb;
+}
+
+ul {
+  margin :0 auto;
+  width: 500px;
+}
+
+li {
+  margin-bottom: 10px;
+  margin-left: -20px;
+  text-align: left;
 }
 </style>
